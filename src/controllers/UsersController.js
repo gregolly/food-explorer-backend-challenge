700f3272a668b04ed1f1c803bf9f1cca85e0fe03
gregolly <gregollyff@hotmail.com>
2024-01-04 08:24:09 -0400

    feat: changed variable HasEmailExist and update users info

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,13 +6,13 @@ class UsersController {
     async create(request, response) {
         const { name, email, password, isAdmin } = request.body
         
-        const IsUserCreated = await knex('users').where({ email }).first()
+        const HasEmailExist = await knex('users').where({ email }).first()
 
         if (!name || !email || !password) {
             throw new AppError('Informe todos os campos!')
         }
 
-        if (IsUserCreated) {
+        if (HasEmailExist) {
             throw new AppError("Este e-mail ja esta em uso!")
         }
 
@@ -30,6 +30,50 @@ class UsersController {
         })
 
         return response.status(201).json()
+    }
+
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body
+        const user_id = request.user.id
+
+        const user = await knex('users').where({ id: user_id }).first()
+
+        if (!user) {
+            throw new AppError('Usuario nao existe')
+        }
+
+        if (email !== user.email) {
+            const userWithUpdatedEmail = await knex('users').where({ email }).first()
+    
+            if (userWithUpdatedEmail) {
+                throw new AppError('Este email ja esta em uso')
+            }
+        }
+
+        user.name = name ?? user.name
+        user.email = email ?? user.email
+
+        if(password && !old_password) {
+            throw new AppError('Voce precisa informar a senha antiga para definir a nova senha')
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword) {
+                throw new AppError('A senha antiga nao confere!')
+            }
+
+            user.password = await hash(password, 8)
+        }
+
+        await knex('users').where({ id: user_id }).update({
+            name: name || user.name,
+            email: email || user.email,
+            password: user.password,
+        })
+
+        return response.json()
     }
 }
 
