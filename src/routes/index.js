6ddfa587f499d91625a54c6995cb5cccffec9a62
gregolly <gregollyff@hotmail.com>
2024-01-04 08:24:21 -0400

    feat: add new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
+const sessionRouter = require("./sessions.routes")
 
 const router = Router()
 
 router.use('/users', usersRouter)
+router.use("/sessions", sessionRouter)
 
 module.exports = router
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const ensureUsersIsAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const UsersController = require('../controllers/UsersController')
 const usersController = new UsersController()
@@ -6,5 +7,6 @@ const usersController = new UsersController()
 const usersRoutes = Router()
 
 usersRoutes.post('/', usersController.create)
+usersRoutes.put("/", ensureUsersIsAuthenticated, usersController.update)
 
 module.exports = usersRoutes
